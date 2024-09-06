e588659ae3e4c35231b1590f40696505e73f54e3
Ram Kishore Prajapati <shekhar.prajapat.ht@gmail.com>
2024-09-06 15:55:01 +0530

    Flow and Channel - I have implemented flow.

--- Flows and Channel/app/src/main/java/com/rk/flowsandchannel/MainActivity.kt
+++ Flows and Channel/app/src/main/java/com/rk/flowsandchannel/MainActivity.kt
@@ -16,14 +16,33 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.rk.flowsandchannel.ui.theme.FlowsAndChannelTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    //Channel is basically hot. that means it providing continuously data. if consumer consume or not.
+    //Channel - Channel is basically hot. that means it providing continuously data. if consumer consume or not.
+    //Flow -  Flow is basically cold. that means it providing data only when consumer consume. It will provide all of the data one by one even flow started already
     var channel = Channel<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /*channelProducer()
+          channelConsumer()*/
+        val job = GlobalScope.launch {
+            val data : Flow<Int> = producerFlow()
+            data.collect {
+                Log.d("TAG", "onCreate: $it")
+            }
+        }
+
+        GlobalScope.launch {
+            delay(2500)
+            job.cancel()
+        }
+
         enableEdgeToEdge()
         setContent {
             FlowsAndChannelTheme {
@@ -31,8 +50,7 @@ class MainActivity : ComponentActivity() {
                     Box(
                         modifier = Modifier.padding(innerPadding)
                     ) {
-                        channelProducer()
-                        channelConsumer()
+
                     }
                 }
             }
@@ -51,6 +69,14 @@ class MainActivity : ComponentActivity() {
 
             Log.d("TAG", "channelConsumer: " + channel.receive().toString())
             Log.d("TAG", "channelConsumer: " + channel.receive().toString())
+        }
+    }
+
+    private fun producerFlow()= flow {
+        val list = listOf(1,2,3,4,5)
+        list.forEach {
+            delay(1000)
+            emit(it)
         }
     }
 
