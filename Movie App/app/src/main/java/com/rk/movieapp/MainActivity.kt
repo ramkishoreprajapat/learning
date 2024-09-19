f9bab195d9a37f8a96ad2789778aa4dac119388e
Ram Kishore Prajapati <shekhar.prajapat.ht@gmail.com>
2024-09-19 16:26:30 +0530

    Movie App - Initial Commits

---
+++ Movie App/app/src/main/java/com/rk/movieapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.rk.movieapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.rk.movieapp.ui.theme.MovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MovieAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box (
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+
+                    }
+                }
+            }
+        }
+    }
+}
